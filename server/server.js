39676386e03e9e0c68e748505402ee2b011b1fae
Jonathan Romero <jonathan.jjrt@gmail.com>
2018-10-13 13:25:29 -0500

    Creando objeto usuario y borrando post emulacion

--- server/server.js
+++ server/server.js
@@ -1,51 +1,29 @@
 require('./config/config');
 
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(require('./routes/usuario'));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
-app.post('/usuario', function(req, res) {
 
-    let body = req.body;
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
 
-    if (body.nombre === undefined) {
+    if (err) throw err;
 
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-    res.json({
-        persona
-    });
+    console.log('Bases de datos ONLINE');
 
 });
-app.put('/usuario/:id', function(req, res) {
 
-    let id = req.params.id;
-
-    res.json({ id });
-});
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
 
 app.listen(process.env.PORT, () => {
-    console.log("escuchando el puerto:3000");
+    console.log("escuchando el puerto:", process.env.PORT);
 });
