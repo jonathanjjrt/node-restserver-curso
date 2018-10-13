eebec37c52e5f93d05ef8d5ba18e6fa91d49a508
Jonathan Romero <jonathan.jjrt@gmail.com>
2018-10-13 13:28:08 -0500

    borrado de datos post recividos

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -26,28 +26,9 @@ app.post('/usuario', function(req, res) {
         res.json({
             ok: true,
             usuario: usuarioDB
-        })
-
-    })
-
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
         });
 
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-    res.json({
-        persona
     });
-
 });
 app.put('/usuario/:id', function(req, res) {
 
